fc65e75ada22bad868448bcc0d6270fe85dc4954
Aloento <11802769+Aloento@users.noreply.github.com>
2023-04-20 22:26:46 +0200

    LengthRegulator

--- models.py
+++ models.py
@@ -835,7 +835,7 @@ class LengthRegulator(nn.Module):
 
     return out
 
-  def forward(self, x, duration, max_len):
+  def forward(self, x, duration, max_len=None):
     output, mel_len = self.LR(x, duration, max_len)
     return output, mel_len
 
@@ -1082,7 +1082,7 @@ class SynthesizerTrn(nn.Module):
     l_length = l_loss / torch.sum(x_mask)
 
     # 音素级别转换成帧级
-    x_frame, x_lengths = self.lr(x, phndur, phonemes_lengths)
+    x_frame, x_lengths = self.lr(x, phndur)
     x_frame = x_frame.to(x.device)
     x_mask = torch.unsqueeze(commons.sequence_mask(x_lengths, x_frame.size(2)), 1).to(x.dtype).to(x.device)
 
@@ -1131,7 +1131,7 @@ class SynthesizerTrn(nn.Module):
     w = torch.exp(logw) * x_mask * length_scale
     duration = torch.ceil(w)
 
-    x_frame, x_lengths = self.lr(x, duration, phonemes_lengths)
+    x_frame, x_lengths = self.lr(x, duration)
     x_frame = x_frame.to(x.device)
     x_mask = torch.unsqueeze(commons.sequence_mask(x_lengths, x_frame.size(2)), 1).to(x.dtype).to(x.device)
 
@@ -1170,7 +1170,7 @@ class SynthesizerTrn(nn.Module):
     w = torch.exp(logw) * x_mask * length_scale
     duration = torch.ceil(w)
 
-    x_frame, x_lengths = self.lr(x, duration, phonemes_lengths)
+    x_frame, x_lengths = self.lr(x, duration)
     x_frame = x_frame.to(x.device)
     x_mask = torch.unsqueeze(commons.sequence_mask(x_lengths, x_frame.size(2)), 1).to(x.dtype).to(x.device)
 
